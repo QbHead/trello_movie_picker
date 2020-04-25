0f9aa95744b6dea79b0ee47ae579b0da3fca698a
Gábor Könye <konye.gabor98@gmail.com>
2020-04-26 00:13:46 +0300

    cleaning

--- files/get_movie.py
+++ files/get_movie.py
@@ -1,21 +1,19 @@
-import apikey
 import requests
-import json
 import random
+import apikey
+import json
 
 my_token=apikey.my_token
 my_key=apikey.my_key
 src_by_board_name=apikey.src_by_board_name
 src_by_table_name=apikey.src_by_table_name
-board_id=""
-table_id=""
 
 def get_board_ids():
     url = 'https://api.trello.com/1/members/me/boards?key='+my_key+'&token='+my_token
     json_data=run_get(url)
     return search_name(json_data,src_by_board_name)
 
-def get_list_ids():
+def get_list_ids(board_id):
     url = 'https://api.trello.com/1/boards/'+ board_id +'/lists?key='+my_key+'&token='+my_token
     json_data=run_get(url)
     return search_name(json_data,src_by_table_name)
@@ -26,7 +24,7 @@ def search_name(data,name):
             return data[y]["id"]
     return 0
 
-def get_card_names():
+def get_card_names(table_id):
     url = 'https://api.trello.com/1/lists/'+table_id+'/cards?key='+my_key+'&token='+my_token
     cards=run_get(url)
     names=[]
@@ -42,11 +40,8 @@ def run_get(url):
     return json.loads(response.text)
 
 def get_random_film():
-    global board_id
-    global table_id
-
     board_id = get_board_ids()
-    table_id = get_list_ids()
-    get_card_names()
+    table_id = get_list_ids(board_id)
+    get_card_names(table_id)
     
 get_random_film()
